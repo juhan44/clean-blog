1b4637a18a68d2b9ef60e88faf50d5df061e9800
Peter Kiss <peter.kiss3@student.ukf.sk>
2026-04-20 20:06:02 +0200

    Header a footer

---
+++ public/templates/partials/footer-admin.php
@@ -0,0 +1,8 @@
+ <footer class="footer">
+            <p>&copy; 2026 DayNight Admin. Designed by <a href="https://www.templatemo.com" target="_blank" rel="nofollow">TemplateMo</a></p>
+        </footer>
+    </div>
+
+    <script src="../assets/js/templatemo-daynight-script.js"></script>
+</body>
+</html>
